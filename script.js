98ad71ddc27521f96ae17c886cb91117c72cf5e1
Jake Greenberg-Bell <jacobgreenbergbell@students.berkeley.net>
2018-05-16 11:38:01 -0700

    third

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
-    $("img").fadeIn(6000);
-    $("#backgroundWriting").fadeOut(8000);
+    $("img").fadeIn(4000);
+    $("#credit").fadeIn(3000);
+    $("#moreInfo").fadeIn(3000);
+    $("#backgroundWriting1").fadeOut(8000);
+    $("#backgroundWriting2").fadeOut(10000);
 
 
     $("img").on("click", function (){
+        $("#bioTable").show();
+        $("#mask").show();
+    });
+
+    $("#mask").on("click", function(){
+       $("#mask").hide();
+       $("#bioTable").hide();
+
     });
 
 
 });
+
+
+function Person(name, years, crime){
+    this.name = name;
+    this.years = years;
+    this.crime = crime;
+}

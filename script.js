f3b674b17c9fa6264e4acb80874c3ff48b0f4945
Jake Greenberg-Bell <jacobgreenbergbell@students.berkeley.net>
2018-05-25 11:36:59 -0700

    six

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
     $("img").fadeIn(4000);
     $("#credit").fadeIn(3000);
     $("#moreInfo").fadeIn(3000);
-    $("#backgroundWriting1").fadeOut(8000);
+    $("#backgroundWriting1").fadeOut(10000);
     $("#backgroundWriting2").fadeOut(10000);
 
 
     $("img").on("click", function (){
+        $(this).val();
         $("#bioTable").show();
         $("#mask").show();
     });
@@ -37,7 +38,7 @@ function Person(name, years, crime){
 }
 
 var AnthonyGraves = new Person("Anthony Graves", 18, "convicted of murdering a family of six people");
-var AnthonyHinton = new Person("Anthony Ray Hinton", 28,"wrongly convicted of the murders of two restaurant workers in Birmingham, Alabama in 1985" ):
+var AnthonyHinton = new Person("Anthony Ray Hinton", 28,"wrongly convicted of the murders of two restaurant workers in Birmingham, Alabama in 1985" );
 var CurtisMccarty = new Person("Curtis Edward McCarty", 21, "for a 1982 Oklahoma City murder he didn’t commit");
 var DerralWayneHodgkins = Person("Derral Wayne Hodgkins", 9, "Murder of friend Teresa Lodge");
 var DerrickJamison = Person("Derrick Jamison", 20, "falsely testified against that he had committed the murder");
